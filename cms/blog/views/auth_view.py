01a48d4119c10afd6fe9dcad1b26f74e178f237f
unikat7 <tamangunika38@gmail.com>
2024-10-15 21:03:55 +0545

    image insertion and setting session

--- cms/blog/views/auth_view.py
+++ cms/blog/views/auth_view.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login as LOGIN
 
 
 def register(request):
@@ -20,4 +21,14 @@ def register(request):
 
 
 def login(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            LOGIN(request,user)
+            return redirect("home")
+        else:
+            return redirect("login")
+
     return render(request,'auth/login.html')
--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -123,6 +123,8 @@ import os
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+LOGIN_URL="/blog/login"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
